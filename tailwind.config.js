2b944094774b657ec16d5da1e7ff4a6320df128d
Rommel <cunetarommel0620@gmail.com>
2025-07-05 11:59:29 +0800

    fix path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./*.js",], // 👈 include your HTML file(s)
+  content: ["./public/**/*.html", "./public/**/*.js"],
   theme: {
     extend: {},
   },
